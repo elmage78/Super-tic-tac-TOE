48e0126b2c9183a237f99e86fb0c40f75967fb80
elmage78 <99903003+elmage78@users.noreply.github.com>
2024-10-02 14:43:48 +0200

    Files v1

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include "TIcTacToe.h"
+#include <ctype.h>
+
+using namespace std;
+
+
+int main()
+{
+    MainBoard CurrentBoard;
+    bool PlayinX = false;
+    int Xboard,Yboard;
+    while(CurrentBoard.WhoWon == 0){
+        if(CurrentBoard.SpecialMove == 1){cout << "Select which board would you like to play in x and y coordinates X: "; cin>>Xboard;cout<< " y: ";cin>>Yboard;}
+        else
+        {
+            Xboard = CurrentBoard.LastPlayedX;
+            Yboard = CurrentBoard.LastPlayedY;
+        }
+        if (Xboard > 3 || Xboard <= 0 || Yboard > 3 || Yboard <= 0){
+            cout << "that coordinate is not valid, select a coordinate ranging 1-3 in x and y";
+        }
+        if (CurrentBoard.Whole[Yboard-1][Xboard-1] != 0){
+            cout << "that board is already won select another one \n\n";
+            continue;
+        }
+        CurrentBoard.PlayWherever(PlayinX,Xboard,Yboard);
+        if (CurrentBoard.SpecialMove == 1){
+            continue;
+        }
+        PlayinX = !PlayinX;
+    }
+    return 0;
+}
